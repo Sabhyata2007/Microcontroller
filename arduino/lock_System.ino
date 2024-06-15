4c21f4defc46bb55bbf4f462d3e75be15eb058c7
Sabhyata Soni <143525662+Sabhyata2007@users.noreply.github.com>
2024-06-16 03:10:17 +0530

    Update lock_System.ino

--- arduino/lock_System.ino
+++ arduino/lock_System.ino
@@ -1,23 +1,15 @@
-// two switch will be there for selecting keypad or rfid sensor 
 
-int value1; //fp
-const int buzzer = 7;// fp
 #include <Keypad.h>
 #include <Servo.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#define SS_PIN 10
-#define RST_PIN 5
-#define ROW_NUM    4 
-#define COLUMN_NUM 4 
+int value1; //fp
 #define SERVO_PIN  A0 
-#define SS_PIN 10 
-#define RST_PIN 5 
-MFRC522 rfid(SS_PIN, RST_PIN);// rfid function using these pins 
-byte keyTagUID[4] = {0xFF, 0xFF, 0xFF, 0xFF};
-
+const int buzzer = A1;// fp
+#define ROW_NUM    4 
+#define COLUMN_NUM 4  
 Servo servo; // servo motor
-
+ int switch_value = A2
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1', '2', '3', 'A'},
   {'4', '5', '6', 'B'},
@@ -39,82 +31,21 @@ int angle = 0; // the current angle of servo motor
 unsigned long lastTime;
 
 void setup() {
- 
-  // rfid scanner( firstly generate the uid code for card from the blue token and then use the card to unlock the system )
-  if(switch one is pressed){
-   Serial.begin(9600);
-   SPI.begin(); // init SPI bus
-   rfid.PCD_Init(); // init MFRC522
-   servo.attach(SERVO_PIN);
-   servo.write(0); // rotate servo motor to 0°
-   lastTime = millis();
-   Serial.println("Tap RFID/NFC Tag on reader");
-  }
-
-
-  //Generate a function for keypad sensor 
-  else if(switch two is pressed){
   Serial.begin(9600);
   input_password.reserve(32); // maximum password size is 32, change if needed
 
   servo.attach(SERVO_PIN);
   servo.write(0); // rotate servo motor to 0°
-  lastTime = millis();
-  }
-
-
-  else{ // fingerprint sensors 
-  Serial.begin(9600);
+  lastTime = millis(); // fingerprint sensors 
   pinMode(buzzer, OUTPUT); 
-  servo.attach(SERVO_PIN);
-  servo.write(0); // rotate servo motor to 0°
-  lastTime = millis();
-  }
+  pinMode(switch_value , OUTPUT);
  
 }
 
-void loop() {
- 
- // generate a function for rfid
- if (switch 1 is pressed){
-   if (rfid.PICC_IsNewCardPresent()) { // new tag is available
-      if (rfid.PICC_ReadCardSerial()) { // NUID has been readed
-            MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-
-             if (rfid.uid.uidByte[0] == keyTagUID[0] &&
-                  rfid.uid.uidByte[1] == keyTagUID[1] &&
-                  rfid.uid.uidByte[2] == keyTagUID[2] &&
-                   rfid.uid.uidByte[3] == keyTagUID[3] ) {
-                                Serial.println("Access is granted");
-                                angle = 90;
-                                servo.write(angle);
-                                lastTime = millis();
-                                
-                                                         }
-            else
-                {
-               Serial.print("Access denied for user with UID:");
-               for (int i = 0; i < rfid.uid.size; i++) {
-               Serial.print(rfid.uid.uidByte[i] < 0x10 ? " 0" : " ");
-               Serial.print(rfid.uid.uidByte[i], HEX);
-                                                        }
-                Serial.println();
-                 }
-
-       rfid.PICC_HaltA(); // halt PICC
-       rfid.PCD_StopCrypto1(); // stop encryption on PCD
-    }
-  }
-     if (angle == 90 && (millis() - lastTime) > 5000) { // 5 seconds
-    angle = 0;
-    servo.write(angle);
-    Serial.println("Rotating Servo Motor to 0°");
-  }
-}
-
-  
-  // generate a function for keypad
-  if(switch 2 is pressed){
+void loop() { 
+ // generate a function for keypad
+  int x = digitalRead(switch_value)
+  if (x==HIGH){
      char key = keypad.getKey();
      if (key) {
         Serial.println(key);
@@ -146,10 +77,10 @@ void loop() {
     Serial.println("Rotating Servo Motor to 0°");
   }
 }
-
-  
+}
+ else{
    // fingerprint sensor 
-   Serial.print("Reading");
+  Serial.print("Reading");
   while(Serial.available()==0);
   value1 = Serial.read();
   Serial.print(value1);
