99a8122a8ea4c50aefaca407fa39529ea73c8af9
Sabhyata Soni <143525662+Sabhyata2007@users.noreply.github.com>
2024-06-21 09:27:08 +0530

    Update new.ino

--- arduino/new.ino
+++ arduino/new.ino
@@ -1,6 +1,6 @@
 #include <Keypad.h>
 #include <Servo.h>
-
+#include <LiquidCrystal.h>
 #define SERVO_PIN 5
 #define buzzer A0
 #define SWITCH_PIN A1
@@ -12,6 +12,9 @@ Servo myservo;
 int pos = 0;
 const int ROW_NUM = 4; // Four rows
 const int COLUMN_NUM = 4; // Four columns
+const int rs = 3, en = 2, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 char keys[ROW_NUM][COLUMN_NUM] = {
   {'1', '2', '3', 'A'},
@@ -37,12 +40,11 @@ void setup() {
   delay(2000);
   pinMode(buzzer, OUTPUT);
   pinMode(SWITCH_PIN, INPUT);
+  lcd.begin(16, 2); 
 }
 
 void loop() {
   bool switchState = digitalRead(SWITCH_PIN);
-  Serial.print("Switch State: ");
-  Serial.println(switchState);
 
   if (switchState == HIGH) {
     handleKeypadInput();
@@ -60,13 +62,35 @@ void handleKeypadInput() {
     } else if (key == '#') {
       if (input_password == password_1 || input_password == password_2 || input_password == password_3) {
         Serial.println("The password is correct, Door is unlocking");
-        delay(1000);
         digitalWrite(buzzer, HIGH);
         delay(1000);
         digitalWrite(buzzer, LOW);
+         lcd.setCursor(0,0);           //sets the cursor at row 0 column 0
+        lcd.print("DOOR"); // prints 16x2 LCD MODULE
+       lcd.setCursor(2,1);           //sets the cursor at row 1 column 2
+       lcd.print("UNLOCKING"); 
+       delay(1000);
+       or (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
+                myservo.write(pos);                 // tell servo to go to position in variable 'pos'
+                delay(15);                          // waits 15 ms for the servo to reach the position
+                  }
+                delay(1000);                          // delay of 1 second before changing direction of rotation
+                for (pos = 90; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+                myservo.write(pos);                 // tell servo to go to position in variable 'pos'
+                delay(15); }  
+
+      }
+        lcd.clear();
+        delay(1000);
+        
       
       } else {
         Serial.println("The password is incorrect, try again");
+        lcd.setCursor(0,0);           //sets the cursor at row 0 column 0
+        lcd.print("PASSWORD");
+        lcd.setCursor(2,1);           //sets the cursor at row 1 column 2
+       lcd.print("INCORRECT"); 
+        
       }
       input_password = ""; // Reset the input password
     } else {
@@ -82,13 +106,17 @@ void handleBluetoothInput() {
          Serial.print(value1);
         if (value1==1)
          { 
-        Serial.print("Unlocking");                     
-
-       digitalWrite(buzzer, HIGH); 
+        Serial.print("Unlocking"); 
+        digitalWrite(buzzer, HIGH); 
        delay(500);        
       digitalWrite(buzzer, LOW); 
       delay(1000); 
-      for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
+         lcd.setCursor(0,0);           //sets the cursor at row 0 column 0
+        lcd.print("DOOR"); // prints 16x2 LCD MODULE
+       lcd.setCursor(2,1);           //sets the cursor at row 1 column 2
+       lcd.print("UNLOCKING");
+       delay(1000);
+        for (pos = 0; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees in steps of 1 degree
                 myservo.write(pos);                 // tell servo to go to position in variable 'pos'
                 delay(15);                          // waits 15 ms for the servo to reach the position
                   }
@@ -97,6 +125,11 @@ void handleBluetoothInput() {
                 myservo.write(pos);                 // tell servo to go to position in variable 'pos'
                 delay(15); }  
 
+      lcd.clear();
+      delay(1000)  ;                   
+
+       
+     
       }
     if (value1==0)
     {
